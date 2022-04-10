af8cb5cfa3d8c709416d20e19af8b786b2f5f5c5
gbengaoyetade <ioyetade@gmail.com>
2022-04-10 17:33:29 +0100

    modify implementation

--- udagram-frontend/src/app/feed/feed-upload/feed-upload.component.ts
+++ udagram-frontend/src/app/feed/feed-upload/feed-upload.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 import { FeedProviderService } from '../services/feed.provider.service';
 
 import { LoadingController, ModalController } from '@ionic/angular';
-
 
 @Component({
   selector: 'app-feed-upload',
@@ -21,16 +25,16 @@ export class FeedUploadComponent implements OnInit {
     private formBuilder: FormBuilder,
     private loadingController: LoadingController,
     private modalController: ModalController
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.uploadForm = this.formBuilder.group({
-      caption: new FormControl('', Validators.required)
+      caption: new FormControl('', Validators.required),
     });
   }
 
   setPreviewDataUrl(file: Blob) {
-    const reader  = new FileReader();
+    const reader = new FileReader();
     reader.onloadend = () => {
       this.previewDataUrl = reader.result;
     };
@@ -46,16 +50,19 @@ export class FeedUploadComponent implements OnInit {
     }
     this.file = file[0];
     this.setPreviewDataUrl(this.file);
-
   }
 
   onSubmit($event) {
     $event.preventDefault();
     this.loadingController.create();
 
-    if (!this.uploadForm.valid || !this.file) { return; }
-    this.feed.uploadFeedItem(this.uploadForm.controls.caption.value, this.file)
+    if (!this.uploadForm.valid || !this.file) {
+      return;
+    }
+    this.feed
+      .uploadFeedItem(this.uploadForm.controls.caption.value, this.file)
       .then((result) => {
+        console.log(result);
         this.modalController.dismiss();
         this.loadingController.dismiss();
       });
